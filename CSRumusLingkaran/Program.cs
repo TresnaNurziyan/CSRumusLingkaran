31f2eae1dc4d1350c9dfc18a57e2fe357ee0c856
Tresna Nurziyan <tresna.nurziyan@gmail.com>
2017-07-26 11:35:00 +0700

    Aplikasi menghitung bangun datar lingkaran

---
+++ CSRumusLingkaran/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSRumusLingkaran
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int jari;
+
+            Console.Write("Masukan jari-jari lingkaran : ");
+            jari = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Luas Lingkaran       : " + 22 / 7 * (jari * jari));
+            Console.WriteLine("Keliling Lingkaran   : " + 2 * 22 / 7 * jari);
+            Console.WriteLine("-----------------------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
